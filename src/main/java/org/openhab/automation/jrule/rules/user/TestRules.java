b4bc68abec2b031621da6e469aeef5abb5e99fa1
querdenker2k <querdenker2k@gmx.de>
2022-10-25 17:59:12 +0200

    update

--- src/main/java/org/openhab/automation/jrule/rules/user/TestRules.java
+++ src/main/java/org/openhab/automation/jrule/rules/user/TestRules.java
@@ -1,102 +1,119 @@
 package org.openhab.automation.jrule.rules.user;
 
 import org.openhab.automation.jrule.generated.actions.JRuleActions;
-import org.openhab.automation.jrule.generated.items._MyMqttTrigger;
+import org.openhab.automation.jrule.generated.items.JRuleItems;
+import org.openhab.automation.jrule.generated.items.*;
 import org.openhab.automation.jrule.generated.things.JRuleThings;
 import org.openhab.automation.jrule.items.JRuleSwitchItem;
 import org.openhab.automation.jrule.rules.JRule;
-import org.openhab.automation.jrule.rules.JRuleEvent;
+import org.openhab.automation.jrule.rules.JRuleCondition;
 import org.openhab.automation.jrule.rules.JRuleName;
-import org.openhab.automation.jrule.rules.JRuleWhen;
+import org.openhab.automation.jrule.rules.JRulePrecondition;
+import org.openhab.automation.jrule.rules.JRuleTag;
+import org.openhab.automation.jrule.rules.JRuleWhenChannelTrigger;
+import org.openhab.automation.jrule.rules.JRuleWhenCronTrigger;
+import org.openhab.automation.jrule.rules.JRuleWhenItemChange;
+import org.openhab.automation.jrule.rules.JRuleWhenItemReceivedCommand;
+import org.openhab.automation.jrule.rules.JRuleWhenThingTrigger;
+import org.openhab.automation.jrule.rules.JRuleWhenTimeTrigger;
+import org.openhab.automation.jrule.rules.event.JRuleChannelEvent;
+import org.openhab.automation.jrule.rules.event.JRuleEvent;
+import org.openhab.automation.jrule.rules.event.JRuleItemEvent;
+import org.openhab.automation.jrule.rules.event.JRuleThingEvent;
+import org.openhab.automation.jrule.rules.value.JRuleOnOffValue;
+import org.openhab.automation.jrule.things.JRuleThingStatus;
+import org.openhab.automation.jrule.trigger.JRuleSwitchTrigger;
+
+import static org.openhab.automation.jrule.trigger.JRuleSwitchTrigger.ON;
 
 public class TestRules extends JRule {
-//    @JRuleName("MyRuleTurnSwitch2On")
-//    @JRuleWhenItemChange(item = _MyTestSwitch.ITEM, to = ON)
-//    @JRuleTag({"item", "change"})
-//    public void myRuleTurnSwitch2On(JRuleEvent event) {
-//        logInfo("triggered with item change: {}, state: {}, oldState: {}",
-//                ((JRuleItemEvent) event).getItemName(), ((JRuleItemEvent) event).getState(), ((JRuleItemEvent) event).getOldState());
-//        JRuleItems.MyTestSwitch2.sendCommand(JRuleOnOffValue.ON);
-//    }
-//
-//    @JRuleName("MyEventValueTest")
-//    @JRuleWhenItemReceivedCommand(item = _MyTestSwitch2.ITEM)
-//    public void myEventValueTest(JRuleEvent event) {
-//        logInfo("Got value from event: {}", ((JRuleItemEvent) event).getState().getValue());
-//    }
-//
-//    @JRuleName("MyNumberRule1")
-//    @JRuleWhenItemChange(item = _MyTestNumber.ITEM, from = "14", to = "10")
-//    @JRuleWhenItemChange(item = _MyTestNumber.ITEM, from = "10", to = "12")
-//    public void myOrRuleNumber(JRuleEvent event) {
-//        logInfo("Got change number: {}", ((JRuleItemEvent) event).getState().getValue());
-//    }
-//
-//    @JRuleName("TestExecutingCommandLine")
-//    @JRuleWhenItemReceivedCommand(item = _MySwitchGroup.ITEM)
-//    public synchronized void testExecutingCommandLine(JRuleEvent event) {
-//        logInfo("Creating dummy file using CLI");
-//        executeCommandLine("touch", "/openhab/userdata/example.txt");
-//    }
-//
-//    @JRuleName("ChannelTriggered")
-//    @JRuleWhenChannelTrigger(channel = "mqtt:topic:mqtt:generic:numberTrigger")
-//    public synchronized void channelTriggered(JRuleEvent event) {
-//        logInfo("Channel triggered with value: {}", ((JRuleChannelEvent) event).getEvent());
-//    }
-//
-//    @JRulePrecondition(item = _MyTestDisturbanceSwitch.ITEM, condition = @JRuleCondition(eq = "ON"))
-//    @JRuleName("MyTestPreConditionRule1")
-//    @JRuleWhenItemReceivedCommand(item = _MyMessageNotification.ITEM)
-//    public void testPrecondition(JRuleEvent event) {
-//        String notificationMessage = ((JRuleItemEvent) event).getState().getValue();
-//        logInfo("It is ok to send notification: {}", notificationMessage);
-////        JRuleItems.MySendNoticationItemMqtt.sendCommand(notificationMessage);
-//    }
-//
-//    @JRuleName("Log every thing that goes offline")
-//    @JRuleWhenThingTrigger(to = JRuleThingStatus.OFFLINE)
-//    public void startTrackingNonOnlineThing(JRuleEvent event) {
-//        String offlineThingUID = ((JRuleThingEvent) event).getThing();
-//        String status = ((JRuleThingEvent) event).getStatus();
-//        logInfo("thing '{}' goes '{}'", offlineThingUID, status);
-//    }
-//
-//    @JRuleName("SetDayBrightness")
-//    @JRuleWhenTimeTrigger(hours=22, minutes=30)
-//    public synchronized void setDayBrightness(JRuleEvent event) {
-//        logInfo("Setting night brightness to 30%");
-//        int dimLevel = 30;
-//        JRuleItems.MyDimmerBrightness.sendCommand(dimLevel);
-//    }
-//
-//    @JRuleName("turnOnFanIfTemperatureIsLow")
-//    @JRuleWhenItemChange(item = _MyTemperatureSensor.ITEM, condition = @JRuleCondition(lte = 20))
-//    public synchronized void turnOnFanIfTemperatureIsLow(JRuleEvent event) {
-//        logInfo("Starting fan since temperature dropped below 20");
-//        JRuleItems.MyHeatingFanSwitch.sendCommand(JRuleOnOffValue.ON);
-//    }
-//
-//    @JRuleName("groupMySwitchesChanged")
-//    @JRuleWhenItemChange(item = _MySwitchGroup.ITEM)
-//    public synchronized void groupMySwitchGroupChanged(JRuleEvent event) {
-//        final boolean groupIsOnline = ((JRuleItemEvent) event).getState().getValueAsOnOffValue() == JRuleOnOffValue.ON;
-//        final String memberThatChangedStatus = ((JRuleItemEvent) event).getMemberName();
-//        logInfo("Member that changed the status of the Group of switches: {}", memberThatChangedStatus);
-//    }
-//
-//    @JRuleName("testCron")
-//    @JRuleWhenCronTrigger(cron = "*/5 * * * * *")
-//    public void testCron(JRuleEvent event) {
-//        logInfo("CRON: Running cron from string every 5 seconds: {}", event);
-//    }
-
-    @JRuleName("TestAction")
-    @JRuleWhen(item = _MyMqttTrigger.ITEM, trigger = JRuleSwitchItem.TRIGGER_CHANGED)
-    public void testAction(JRuleEvent event) {
-        JRuleActions.mqttBrokerMqtt.publishMQTT("number/state", "1313131");
-        logInfo("some info after this");
+    @JRuleName("MyRuleTurnSwitch2On")
+    @JRuleWhenItemChange(item = _MyTestSwitch.ITEM, to = JRuleSwitchTrigger.ON)
+    @JRuleTag({"item", "change"})
+    public void myRuleTurnSwitch2On(JRuleEvent event) {
+        logInfo("triggered with item change: {}, state: {}, oldState: {}",
+                ((JRuleItemEvent) event).getItemName(), ((JRuleItemEvent) event).getState(), ((JRuleItemEvent) event).getOldState());
+        JRuleItems.MyTestSwitch2.sendCommand(JRuleOnOffValue.ON);
     }
+
+    @JRuleName("MyEventValueTest")
+    @JRuleWhenItemReceivedCommand(item = _MyTestSwitch2.ITEM)
+    public void myEventValueTest(JRuleEvent event) {
+        logInfo("Got value from event: {}", ((JRuleItemEvent) event).getState().getValue());
+    }
+
+    @JRuleName("MyNumberRule1")
+    @JRuleWhenItemChange(item = _MyTestNumber.ITEM, from = "14", to = "10")
+    @JRuleWhenItemChange(item = _MyTestNumber.ITEM, from = "10", to = "12")
+    public void myOrRuleNumber(JRuleEvent event) {
+        logInfo("Got change number: {}", ((JRuleItemEvent) event).getState().getValue());
+    }
+
+    @JRuleName("TestExecutingCommandLine")
+    @JRuleWhenItemReceivedCommand(item = _MySwitchGroup.ITEM)
+    public synchronized void testExecutingCommandLine(JRuleEvent event) {
+        logInfo("Creating dummy file using CLI");
+        executeCommandLine("touch", "/openhab/userdata/example.txt");
+    }
+
+    @JRuleName("ChannelTriggered")
+    @JRuleWhenChannelTrigger(channel = "mqtt:topic:mqtt:generic:numberTrigger")
+    public synchronized void channelTriggered(JRuleEvent event) {
+        logInfo("Channel triggered with value: {}", ((JRuleChannelEvent) event).getEvent());
+    }
+
+    @JRulePrecondition(item = _MyTestDisturbanceSwitch.ITEM, condition = @JRuleCondition(eq = "ON"))
+    @JRuleName("MyTestPreConditionRule1")
+    @JRuleWhenItemReceivedCommand(item = _MyMessageNotification.ITEM)
+    public void testPrecondition(JRuleEvent event) {
+        String notificationMessage = ((JRuleItemEvent) event).getState().getValue();
+        logInfo("It is ok to send notification: {}", notificationMessage);
+//        JRuleItems.MySendNoticationItemMqtt.sendCommand(notificationMessage);
+    }
+
+    @JRuleName("Log every thing that goes offline")
+    @JRuleWhenThingTrigger(to = JRuleThingStatus.OFFLINE)
+    public void startTrackingNonOnlineThing(JRuleEvent event) {
+        String offlineThingUID = ((JRuleThingEvent) event).getThing();
+        String status = ((JRuleThingEvent) event).getStatus();
+        logInfo("thing '{}' goes '{}'", offlineThingUID, status);
+    }
+
+    @JRuleName("SetDayBrightness")
+    @JRuleWhenTimeTrigger(hours=22, minutes=30)
+    public synchronized void setDayBrightness(JRuleEvent event) {
+        logInfo("Setting night brightness to 30%");
+        int dimLevel = 30;
+        JRuleItems.MyDimmerBrightness.sendCommand(dimLevel);
+    }
+
+    @JRuleName("turnOnFanIfTemperatureIsLow")
+    @JRuleWhenItemChange(item = _MyTemperatureSensor.ITEM, condition = @JRuleCondition(lte = 20))
+    public synchronized void turnOnFanIfTemperatureIsLow(JRuleEvent event) {
+        logInfo("Starting fan since temperature dropped below 20");
+        JRuleItems.MyHeatingFanSwitch.sendCommand(JRuleOnOffValue.ON);
+    }
+
+    @JRuleName("groupMySwitchesChanged")
+    @JRuleWhenItemChange(item = _MySwitchGroup.ITEM)
+    public synchronized void groupMySwitchGroupChanged(JRuleEvent event) {
+        final boolean groupIsOnline = ((JRuleItemEvent) event).getState().getValueAsOnOffValue() == JRuleOnOffValue.ON;
+        final String memberThatChangedStatus = ((JRuleItemEvent) event).getMemberName();
+        logInfo("Member that changed the status of the Group of switches: {}", memberThatChangedStatus);
+    }
+
+    @JRuleName("testCron")
+    @JRuleWhenCronTrigger(cron = "*/5 * * * * *")
+    public void testCron(JRuleEvent event) {
+        logInfo("CRON: Running cron from string every 5 seconds: {}", event);
+    }
+
+//    @JRuleName("TestAction")
+//    @JRuleWhenItemChange(item = _MyMqttTrigger.ITEM)
+//    public void testAction(JRuleEvent event) {
+//        JRuleActions.mqttBrokerMqtt.publishMQTT("number/state", "1313131");
+//        logInfo("some info after this");
+//    }
 
 
 //    @JRuleName("trigger with channel")
